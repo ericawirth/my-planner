69501eed34ac84757fa2ea8d8859da99965ee852
Jeremy Venerella <jvenerella@Jeremys-MacBook-Pro.local>
2020-11-18 22:14:54 -0500

    calendar header

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from "@fullcalendar/interaction"
 
 
 export default function Calendar() {
-    const [event, setEvent] = useState([{title: 'Smoke & Turkey with KMP', date: '2020-11-24'}]);
+    const [event, setEvent] = useState([{ title: 'Smoke & Turkey with KMP', date: '2020-11-24' }]);
     const handleDateClick = (arg) => {
-        setEvent(oldArray => [...oldArray, {title: 'test', date: arg.dateStr}])
+        setEvent(oldArray => [...oldArray, { title: ' test ', date: arg.dateStr }])
     }
     return (
-        <div>
+        <div className="calendar">
             <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                initialView='timeGridWeek'
                 dateClick={handleDateClick}
                 selectable={true}
                 editable={true}
                 eventContent={renderEventContent}
                 events={event}
+                headerToolbar={{
+                    left: 'today myCustomButton',
+                    center: 'title',
+                    right: 'prev,next dayGridMonth,timeGridWeek,timeGridDay'
+                }}
             />
         </div>
     );
