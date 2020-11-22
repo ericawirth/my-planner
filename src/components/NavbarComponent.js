deac1d2819c1d878f64c2b1333dedcabbe55613c
nicmis <nicmis@live.unc.edu>
2020-11-22 00:20:46 -0500

    loginview and navbar updates

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -6,45 +6,44 @@ export default function NavbarComponent() {
     return (
         <div className="navBar">
             <div className="name">MyPlanner.</div>
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"  href="#">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
-            <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-start">
-                    <a className="navbar-item">
-                        Home
+            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="#">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <a className="navbar-item">
+                            Dashboard
       </a>
-                    <a className="navbar-item">
-                        Documentation
+                        <a className="navbar-item">
+                            Calendar
       </a>
-
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link" href="#">
-                            More
+                        <a className="navbar-item">
+                            To-Do List
+      </a>
+                        <div className="navbar-item has-dropdown is-hoverable">
+                            <a className="navbar-link" href="#">
+                                My Account
         </a>
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item" href="#">
-                                About
+                            <div className="navbar-dropdown">
+                                <a className="navbar-item" href="#">
+                                    Profile
           </a>
-                            <a className="navbar-item" href="#">
-                                Jobs
+                                <a className="navbar-item" href="#">
+                                    Log Out
           </a>
-                            <a className="navbar-item" href="#">
-                                Contact
+                                <a className="navbar-item" href="#">
+                                    Report an issue
           </a>
-                            <a className="navbar-item" href="#">
-                                Report an issue
-          </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         </div>
     )
 }
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -4,59 +4,66 @@ import '../App.sass';
 
 export default function LoginView() {
     return (
-        <div className="loginContainer">
-            <section className="">
-                <div className="container">
-                    <div className="">
-                        <div className="column">
+        <div className="container mt-5">
+            <div className="container">
+                <div className="tile is-ancestor">
+                    <div className="tile is-parent is-vertical is-12">
+                        <div className="tile is-child is-vertical notification is-primary">
+                            <h1 className="title">Welcome,</h1>
+                            <h2 className="subtitle">Please login to access your planner!</h2>
+                            <figure className="image is-3by1">
+                            <img src="https://bulma.io/images/placeholders/640x480.png"></img>
+                            </figure>
+                        </div>
+                        <div className="tile is-child notification is-warning">
                             <h1 className="loginlabel">Login</h1>
                             <form>
                                 <div className="field">
-                                    <label className="label">Username</label>
+                                    <label className="label1">Username:</label>
                                     <div className="control">
-                                        <input className="input" type="text" placeholder="Cookie Monster" name="username"></input>
+                                        <input className="" type="text" placeholder="checkm8" name="username"></input>
                                     </div>
                                 </div>
                                 <div className="">
-                                    <label className="">Password</label> 
+                                    <label className="label1">Password:</label>
                                     <div className="">
-                                        <input className="" type="password" placeholder="password124" name="password"></input>
+                                        <input className="" type="password" placeholder="*******" name="password"></input>
                                     </div>
                                 </div>
                                 <div className="">
                                     <div className="">
                                         <label className="checkbox">
                                             <input type="checkbox" name="remember"></input>
-                                            Remember your login info?
-                                            </label>
+                                Remember your login info?
+                            </label>
                                     </div>
                                 </div>
-                                <div  className="">
+                                <div className="">
                                     <div className="">
                                         <input className="" type="submit"></input>
                                     </div>
                                 </div>
                             </form>
                         </div>
-                        <div className="">
-                            <h1 className="loginlabel">Signup</h1>
+                        <div className="tile is-child notification is-danger">
+                            <h1 className="signuplabel">Don't have an account? Sign Up!</h1>
                             <form>
                                 <div className="">
-                                    <label className="">Username</label>
+                                    <label className="label1">Create Username:</label>
                                     <div className="">
-                                        <input className="" type="text" placeholder="Cookie Monster  2.0" name="username"></input>                                    
+                                        <input className="" type="text" placeholder="checkm8" name="username"></input>
                                     </div>
                                 </div>
-                                <div  className="">
-                                    <label className="">Password</label>
+                                <div className="label1">
+                                    <label className="">Create Password:</label>
                                     <div className="">
-                                        <input className="" type="password"  placeholder="password12345"></input>
+                                        <input className="" type="password" placeholder="*******"></input>
                                     </div>
                                 </div>
                                 <div className="">
-                                    <label className="">Repeat Password</label>
+                                    <label className="label1">Repeat Password:</label>
                                     <div className="">
-                                        <input className="input" type="password"  placeholder="password12345" name="password"></input>
+                                        <input className="" type="password" placeholder="Password123!" name="password"></input>
                                     </div>
                                 </div>
                                 <div className="">
@@ -68,7 +75,7 @@ export default function LoginView() {
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </div>
     )
 }
