a778a81959c872b6760d40446de9b528ebddfa04
Jeremy V <jer20084@yahoo.com>
2020-11-23 13:30:44 -0500

    fixed deploy issues

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,10 +1,8 @@
 const admin = require('firebase-admin');
 
-var serviceAccount = require('../../my-planner426-firebase-adminsdk-btqdy-ccdbbcbb7f.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+//var serviceAccount = require('../../my-planner426-firebase-adminsdk-btqdy-ccdbbcbb7f.json');
+//{credential: admin.credential.cert(serviceAccount)}
+admin.initializeApp();
 
 const db = admin.firestore();
 
