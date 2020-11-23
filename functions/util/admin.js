8624d9caec05ccf23e33d25a958892f3a427424f
Jeremy V <jer20084@yahoo.com>
2020-11-23 04:10:21 -0500

    gitignore

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-var serviceAccount = require("D:Dev/426Firebase/my-planner426-firebase-adminsdk-btqdy-ccdbbcbb7f.json");
+var serviceAccount = require('D:Dev\\426Firebase\\my-planner426-firebase-adminsdk-btqdy-ccdbbcbb7f.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
