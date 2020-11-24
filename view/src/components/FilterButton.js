23bd86203b4ba592698f022b2fbf66c704f4ab35
Sunaly <dsuna@live.unc.edu>
2020-11-24 14:01:06 -0500

    code organization

--- view/src/components/FilterButton.js
+++ view/src/components/FilterButton.js
@@ -6,9 +6,9 @@ export default function FilterButton(props) {
       type="button"
       className="btn toggle-btn"
       aria-pressed={props.isPressed}
-      onClick={() => props.setFilter(props.taskName)}
+      onClick={() => props.setFilter(props.title)}
     >
-      <span>{props.taskName}</span>
+      <span>{props.title}</span>
     </button>
   );
 }
--- view/src/views/TodoView.js
+++ view/src/views/TodoView.js
@@ -69,6 +69,71 @@ export default function TodoView(props) {
     }
   }
 
+
+  function addTask(task) {
+    authMiddleWare(history);
+    let newDate = task.start? new Date(task.start+'T'+task.time).toISOString(): '';
+    let newTask = {
+      subject: task.subject, title: task.title, end: '', classDetails: '', backgroundColor: '', allDay: false, 
+      start: newDate, time: task.time, completed: false, eventType: 'Todo', body: '',
+    };
+
+    let options = { url: '/event', method: 'post', data: newTask };
+    let responseId = "";
+    const authToken = localStorage.getItem('AuthToken');
+    axios.defaults.headers.common = { Authorization: `${authToken}` };
+    axios(options)
+      .then((e) => {
+        responseId = e.data.id
+        newTask.id = responseId ? responseId : nanoid();
+        setTasks([...tasks, newTask]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const filterList = FILTER_NAMES.map(title => (
+    <FilterButton
+      key={title}
+      title={title}
+      isPressed={title === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const todoList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        subject={task.subject}
+        title={task.title}
+        start={task.start}
+        time={task.time}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+
+    function toggleTaskCompleted(id) {
+      const updatedTasks = tasks.map(task => {
+        // if this task has the same ID as the edited task
+        if (id === task.id) {
+          // use object spread to make a new object
+          // whose `completed` prop has been inverted
+          return { ...task, completed: !task.completed }
+        }
+        return task;
+      });
+      handleEdit(id, updatedTasks);
+      setTasks(updatedTasks);
+    }
+
   function handleEdit(id, editedArr) {
     authMiddleWare(history);
     const editedTask = editedArr.filter(task => {
@@ -95,18 +160,18 @@ export default function TodoView(props) {
     }
   }
 
-  function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map(task => {
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
       // if this task has the same ID as the edited task
       if (id === task.id) {
-        // use object spread to make a new object
-        // whose `completed` prop has been inverted
-        return { ...task, completed: !task.completed }
+        //
+        return { ...task, title: newName }
       }
       return task;
     });
-    handleEdit(id, updatedTasks);
-    setTasks(updatedTasks);
+    handleEdit(id, editedTaskList);
+    setTasks(editedTaskList);
   }
 
   function handleDelete(id) {
@@ -130,70 +195,6 @@ export default function TodoView(props) {
     setTasks(remainingTasks);
     handleDelete(id);
   }
-
-
-  function editTask(id, newName) {
-    const editedTaskList = tasks.map(task => {
-      // if this task has the same ID as the edited task
-      if (id === task.id) {
-        //
-        return { ...task, title: newName }
-      }
-      return task;
-    });
-    handleEdit(id, editedTaskList);
-    setTasks(editedTaskList);
-  }
-
-  const todoList = tasks
-    .filter(FILTER_MAP[filter])
-    .map(task => (
-      <Todo
-        id={task.id}
-        subject={task.subject}
-        title={task.title}
-        start={task.start}
-        time={task.time}
-        completed={task.completed}
-        key={task.id}
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-        editTask={editTask}
-      />
-    ));
-
-  const filterList = FILTER_NAMES.map(taskName => (
-    <FilterButton
-      key={taskName}
-      taskName={taskName}
-      isPressed={taskName === filter}
-      setFilter={setFilter}
-    />
-  ));
-
-  function addTask(task) {
-    authMiddleWare(history);
-    let newDate = task.start? new Date(task.start+'T'+task.time).toISOString(): '';
-    let newTask = {
-      subject: task.subject, title: task.title, end: '', classDetails: '', backgroundColor: '', allDay: false, 
-      start: newDate, time: task.time, completed: false, eventType: 'Todo', body: '',
-    };
-
-    let options = { url: '/event', method: 'post', data: newTask };
-    let responseId = "";
-    const authToken = localStorage.getItem('AuthToken');
-    axios.defaults.headers.common = { Authorization: `${authToken}` };
-    axios(options)
-      .then((e) => {
-        responseId = e.data.id
-        newTask.id = responseId ? responseId : nanoid();
-        setTasks([...tasks, newTask]);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
 
   const tasksNoun = todoList.length !== 1 ? 'Tasks' : 'Task';
   const headingText = `${todoList.length} ${tasksNoun}`;
