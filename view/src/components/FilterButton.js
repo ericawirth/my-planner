5a231f2e9358549d39c75b392fa11d67b99a4c94
Jeremy Venerella <jvenerella@jeremys-mbp.wireless-1x.unc.edu>
2020-11-24 14:52:33 -0500

    things

--- view/src/components/FilterButton.js
+++ view/src/components/FilterButton.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-export default function FilterButton(props) {
-  return (
-    <button
-      type="button"
-      className="btn toggle-btn"
-      aria-pressed={props.isPressed}
-      onClick={() => props.setFilter(props.taskName)}
-    >
-      <span>{props.taskName}</span>
-    </button>
-  );
-}
-=======
 import React from "react";
 
 export default function FilterButton(props) {
@@ -28,4 +12,3 @@ export default function FilterButton(props) {
     </button>
   );
 }
->>>>>>> 23bd86203b4ba592698f022b2fbf66c704f4ab35
